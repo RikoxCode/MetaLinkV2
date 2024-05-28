f0f23fb4f13f2b299fab042f9f4a46de92a85935
Netshlife <111433435+RikoxCode@users.noreply.github.com>
2024-05-29 00:22:10 +0200

    fix; grad (integer) to grad (decimal / float)

--- database/migrations/2024_05_24_174206_create_sheetmusics_table.php
+++ database/migrations/2024_05_24_174206_create_sheetmusics_table.php
@@ -21,8 +21,8 @@ return new class extends Migration
             $table->string('arranger');
             $table->string('publisher');
             $table->string('style');
-            $table->decimal('grad')->default(0);
-            $table->decimal('flex')->default(0);
+            $table->decimal('grad', 8, 1)->default(0);
+            $table->decimal('flex', 8, 1)->default(0);
             $table->time('duration')->default('00:00:00');
             $table->boolean('is_digital')->default(false);
             $table->string('demo_url')->nullable();
